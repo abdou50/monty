afc610163afa3951f58e76592ebe55702c411dea
louay66 <louaylouay20@gmail.com>
2022-05-12 14:31:37 -0700

    jj

--- push.c
+++ push.c
@@ -1,8 +1,15 @@
 #include "monty.h"
+/**
+ * push-push
+ * @stack:stack
+ * @line_number: line number
+ */
 void push(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp;
+
 	temp = malloc(sizeof(stack_t));
+
 	if (temp == NULL)
 	{
 		exit(EXIT_FAILURE);
