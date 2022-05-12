d21c68f3cc9ef743aa0cd7dc31c8b79f0db5e362
louay66 <louaylouay20@gmail.com>
2022-05-12 14:38:50 -0700

    hh

---
+++ help.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * help - because function is more than 40 line
+ **/
+
+void help(void)
+{
+	tst.fb = NULL;
+	tst.line = NULL;
+	tst.ln = 0;
+	tst.stack = NULL;
+	tst.arg = NULL;
+	tst.data = NULL;
+}
