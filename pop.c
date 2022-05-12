737c9202b4c4931aef8fdfa20a853f91bfd5ec98
louay66 <louaylouay20@gmail.com>
2022-05-12 15:06:14 -0700

    hh

--- pop.c
+++ pop.c
@@ -9,15 +9,11 @@
 
 void pop(stack_t **stack, unsigned int line)
 {
-	stack_t *temp, temp2;
+	stack_t *temp, *temp2;
 
 	temp = *stack;
-	if (temp == NULL)
-	{
-		fprintf(stderr, "L<%d>: can't pop an empty stack\n", line);
-		freedom(1);
-	}
-	else 
+
+	if (temp != NULL) 
 	{
 		temp2 = (*temp).next;
 		if (temp2 != NULL)
@@ -25,5 +21,11 @@ void pop(stack_t **stack, unsigned int line)
 		free(temp);
 		temp = temp2;
 		*stack = temp;
+	}
+	else
+
+	{
+		fprintf(stderr, "L<%d>: can't pop an empty stack\n", line);
+		freedom(1);
 	}
 }
