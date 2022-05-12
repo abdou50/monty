1508959870dbbdadb2b40e879e19e6467b1e10ea
abdou50 <abdelkaderraddadi988@gmail.com>
2022-05-12 14:18:39 +0300

    rien

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * nop - rien de special
+ * @stack: unused attribute
+ * @line_number: unused attribute
+ *
+ * Return: void
+ */
+void nop(stack_t **stack __attribute__((unused)),
+		unsigned int line_number __attribute__((unused)))
+{
+}
