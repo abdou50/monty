2850ffd8869490da926cc8e69e59223c5254d205
abdou50 <abdelkaderraddadi988@gmail.com>
2022-05-12 14:06:29 +0300

    l

---
+++ add.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+/**
+ * add - function that adds
+ *@stack: stack
+ *@line_number: number
+ *
+ * Return: void
+ */
+void add(stack_t **stack, unsigned int line_number)
+{
+	if (*stack && (*stack)->next)
+	{
+		x += (*stack)->n + (*stack)->next->n;
+		pop(stack, line_number);
+	}
+	else
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+}
