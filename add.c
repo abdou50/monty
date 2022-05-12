1d798d1fe44802f7a6a5168d504f33483fe11b3a
louay66 <louaylouay20@gmail.com>
2022-05-12 14:30:38 -0700

    l

--- add.c
+++ add.c
@@ -8,11 +8,13 @@
  */
 void add(stack_t **stack, unsigned int line_number)
 {
+	int x = 0;
+
 	if (*stack && (*stack)->next)
 	{
 		x += (*stack)->n + (*stack)->next->n;
 		pop(stack, line_number);
-	}
+		}
 	else
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
